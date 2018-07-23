7390b2025b27490c4cd3e77a692eab203bb8b87d
Karl <starwolf@sbcglobal.net>
2018-07-23 15:25:27 -0700

    Built out the header.

--- portfolioV2/src/App.vue
+++ portfolioV2/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -12,12 +11,44 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/*Load custom fonts */
+@font-face {
+  font-family: 'adam';
+  src: url('fonts/ADAM.CGPRO.woff') format('woff'),
+       url('fonts/ADAM.CGPRO.woff2') format('woff'),
+       url('fonts/ADAM.CGPRO.otf') format('otf');
 }
+
+@font-face {
+  font-family: 'brandon';
+  src: url('fonts/Brandon_bld.woff') format('woff'),
+       url('fonts/Brandon_bld.otf') format('otf');
+}
+
+/*Global resets*/
+*,
+*::before,
+*::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+}
+
+/* Set html box sizing to border-box so when containers are given widths and than margins/padding
+they will still stay at their original given widths! */
+html {
+    box-sizing: border-box;
+    font-size: 62.5%; /* 10px/16px = 62.5% -> 1rem = 10px; */
+}
+
+h1,h2,h3,h4,h5,h6,p {
+  color: white;
+}
+
+h1 {font-size: 6rem;}
+h2 {font-size: 5rem;}
+h3 {font-size: 4rem;}
+h4 {font-size: 2rem;}
+h5 {font-size: 1rem;}
+p {font-size: 1.7rem;}
 </style>
---
+++ portfolioV2/src/components/MainPage.vue
@@ -0,0 +1,169 @@
+<template>
+<div class="container">
+  <div class="header">
+    <div class="header__inner">
+      <h4>- Karol Poniatowski -</h4>
+      <h1>Web dev & Design</h1>
+      <h5>Napa, California</h5>
+    </div>
+  </div>
+
+  <div class="mainBody">
+    <div class="projectsTitle">
+      <h1>My Projects</h1>
+    </div>
+    <div class="mainNavigation">
+        <ul>
+          <li>Websites</li>
+          <li>Code</li>
+          <li>3d</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    <div class="mainBody__inner">
+      <div class="mainBody__projects">
+        <div class="project">
+
+        </div>
+        <div class="project">
+
+        </div>
+        <div class="project">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "MainPage",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  background: #3a3a3a;
+  overflow: hidden;
+}
+.header {
+  height: 80vh;
+  background-color: #3a3a3a;
+
+  &__inner {
+    width: 765px;
+    text-align: center;
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+    margin: 0 auto;
+
+    h1 {
+      font-family: "adam", Helvetica, Arial, sans-serif;
+      letter-spacing: 1.25rem;
+      font-weight: 100;
+      margin-top: 1rem;
+      margin-bottom: 2.5rem;
+    }
+    h4 {
+      font-family: "adam", Helvetica, Arial, sans-serif;
+      letter-spacing: 2.5rem;
+      font-weight: 100;
+      color: #606060;
+    }
+    h5 {
+      font-family: "adam", Helvetica, Arial, sans-serif;
+      letter-spacing: 2.5rem;
+      font-weight: 100;
+      background-color: #2b2b2b;
+      padding: 1.4rem;
+      color: #b2b2b2;
+    }
+
+    &::after {
+      font-family: "adam", Helvetica, Arial, sans-serif;
+      content: "Scroll (down)";
+      color: white;
+      position: absolute;
+      left: 30%;
+      top: 300px;
+      margin-left: 0.125rem;
+      width: 30rem;
+      height: 0.25rem;
+      transform: rotate(-90deg);
+      background-color: #2b2b2b;
+    }
+  }
+}
+
+.mainBody {
+  display: flex;
+  flex-direction: column;
+  background: #494949;
+  max-width: 120rem;
+  height: 100rem;
+  margin: 0 auto;
+
+  .projectsTitle {
+    display: inline-block;
+    width: 200%;
+    position: relative;
+    right: 150%;
+    background: #BCBCBC;
+    margin: 10rem 0;
+    -webkit-clip-path: polygon(0 0, 97% 0, 100% 100%, 0% 100%);
+    clip-path: polygon(0 0, 97% 0, 100% 100%, 0% 100%);
+
+    h1 {
+      padding: 4rem;
+      text-align: right;
+      font-family: "adam", Helvetica, Arial, sans-serif;
+      color: #777777;
+      font-weight: 100;
+      font-size: 4rem;
+      padding-right: 10rem;
+      letter-spacing: 2rem;
+    }
+  }
+  &__inner {
+    top: 20rem;
+  }
+
+  &__projects {
+
+    .project {
+      margin-top: 8rem;
+      width: 100%;
+      height: 25rem;
+      background: #2B2B2B; 
+    }
+  }
+}
+
+.mainNavigation {
+  background: #2B2B2B;
+  max-width: 70rem;
+  align-self: flex-end;
+
+  & ul li {
+    display: inline-block;
+    color: #C6C6C6;
+    font-size: 1.75rem;
+    font-family: "brandon", Helvetica, Arial, sans-serif;
+    text-transform: uppercase;
+    padding: 2.5rem 3.5rem;
+
+    &:hover {
+      background-color: white;
+    }
+  }
+}
+</style>
